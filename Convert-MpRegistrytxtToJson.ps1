8da1bc37e50069811cecfdbf828fcabbbad88f3c
Bob Ma <typical.dao@gmail.com>
2024-07-24 16:04:15 +0800

    First version: Convert MpRegistry.txt to JSON format.

---
+++ Convert-MpRegistrytxtToJson.ps1
@@ -0,0 +1,31 @@
+function Convert-MpRegistrytxtToJson {
+    param(
+        [string]$Path="$PWD\MpRegistry.txt",
+        [string]$OutFile="$PWD\MpRegistry.json"
+    )
+
+    $lines = Get-Content $Path
+    $jsonObject = [ordered]@{}
+
+    # Split text data into lines
+    # $lines = $MpRegistryText -split "\r?\n"
+
+    $currentSection = ""
+    foreach ($line in $lines) {
+        if ($line.StartsWith("Current configuration options for location")){
+            $policy = ($line -split '"')[1]
+            $jsonObject[$policy] = @{}
+        }
+        elseif ($line -match '^\[(.+)\]$') {
+            $currentSection = $matches[1]
+            $jsonObject[$policy][$currentSection] = [ordered]@{}
+        } elseif ($line -match '^\s{4}(\S+)\s+\[.+\]\s+:\s(.+)$') {
+            $key = $matches[1]
+            $value = $matches[2]
+            $jsonObject[$policy][$currentSection][$key] = $value
+        }
+}
+
+    $json = $jsonObject | ConvertTo-Json -Depth 5
+    $json | Out-File -FilePath $OutFile -Force
+}
