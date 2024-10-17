9793fd41a0c3a1bfece517787e8a40bb9aa363f5
Bob Ma <typical.dao@gmail.com>
2024-10-17 11:29:10 +0800

    Fix an issue of PowerShell 5 not having -AsByteStream parameter.

--- Convert-MacDlpPolicyBin.ps1
+++ Convert-MacDlpPolicyBin.ps1
@@ -2,13 +2,14 @@ function Convert-MacDlpPolicyBin {
     # inspired by MDE client analyzer: MDEClientAnalyzer.ps1
     param (
         [string]$Path="$PWD\dlp_policy.bin",
-        [string]$OutFile="$PWD\configuration.txt"
+        [string]$OutFile="$PWD\dlp_policy.json"
     )
     if ((Test-Path $Path) -eq $false ) {
         Write-Host "Policy file not found. Exiting."
         return
     }
-    $byteArray = Get-Content -AsByteStream -Path $Path
+    # $byteArray = Get-Content -AsByteStream -Path $Path
+    $byteArray = [System.IO.File]::ReadAllBytes($Path)
     $memoryStream = New-Object System.IO.MemoryStream(,$byteArray)
     $deflateStream = New-Object System.IO.Compression.DeflateStream($memoryStream,  [System.IO.Compression.CompressionMode]::Decompress)
     $streamReader =  New-Object System.IO.StreamReader($deflateStream, [System.Text.Encoding]::utf8)
